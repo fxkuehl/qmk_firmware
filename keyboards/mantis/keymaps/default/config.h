2b9f043a37e6e4482d9b65ecedeaa3dfa0218d7f
Felix Kuehling <felix.kuehling@gmail.com>
2023-02-19 01:49:46 -0500

    Add Mantis keyboard
    This is an import of the Mantis v0.2 keyboard as a starting point for
    this branch.
    
    Signed-off-by: Felix Kuehling <felix.kuehling@gmail.com>

---
+++ keyboards/mantis/keymaps/default/config.h
@@ -0,0 +1,6 @@
+// Immediate hold on other key press for thumb mod-taps
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+// Permissive hold for Space and finger mod-taps to allow fast combos
+#define PERMISSIVE_HOLD_PER_KEY
+// Mod-taps and layer-taps behave the same way (default on current QMK)
+//#define IGNORE_MOD_TAP_INTERRUPT
